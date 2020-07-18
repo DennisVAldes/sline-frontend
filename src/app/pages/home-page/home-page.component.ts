0ee8fa7297cc46379bafe06e8e7aba27ba9114f9
Dennis Valdes <dennis.valdez@anima.edu.uy>
2020-07-17 23:42:01 -0300

    Fixes

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -29,7 +29,7 @@ export class HomePageComponent implements AfterViewInit {
 
   public loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
   });
 
   constructor(
