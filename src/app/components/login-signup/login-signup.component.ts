a5ea280f2bce30320e29311ffc237f99ca985987
DennisVAldes <dennis.valdes@anima.edu.uy>
2020-08-19 11:50:37 -0300

    Fixes

--- src/app/components/login-signup/login-signup.component.ts
+++ src/app/components/login-signup/login-signup.component.ts
@@ -18,7 +18,6 @@ export class LoginSignupComponent implements OnInit {
 
   constructor(
     private userService: UserService,
-    private router: Router,
     private authService: AuthService,
     notifierService: NotifierService
   ) { 
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -19,6 +19,6 @@ export class UserService {
     
     public createUser = async (newUser: UserDto): Promise<ApiResponse<UserDto>> =>
             this.http
-                .post<ApiResponse<UserDto>>(`${this.apiHost}/users/`, newUser)
+                .post<ApiResponse<UserDto>>(`${this.apiHost}/users/signup`, newUser)
                 .toPromise();
 }
